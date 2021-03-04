1ac694fffbe57cfbcb13d3da4885d21b834d72d6
Alex Vinson <alex.vinson@softvision.com>
2021-03-04 13:02:04 -0800

    Changed security group

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -52,18 +52,18 @@ resource "aws_security_group" "sg_22" {
   name = "sg_22"
   vpc_id = aws_vpc.vpc.id
 
-  # HTTP access from the VPC
-  ingress {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # SSH access from the VPC
   ingress {
       from_port   = 22
       to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from the VPC
+  ingress {
+      from_port   = 80
+      to_port     = 80
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
